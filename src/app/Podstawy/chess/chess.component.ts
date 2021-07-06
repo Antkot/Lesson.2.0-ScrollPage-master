01a6799606f07ececb39540e8b0d10cc5b32961f
Antkot <antoniks11@gmail.com>
2021-07-06 17:35:29 +0200

    podświetlanie figury, bicie wrogich figur

--- src/app/Podstawy/chess/chess.component.ts
+++ src/app/Podstawy/chess/chess.component.ts
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChessComponent implements OnInit {
 
-  tiles: Array<{ letter: string, numeric: string, color: string, figure: string }> = [];
+  tiles: Array<{ letter: string, numeric: string, color: string, figure: string, figureColor }> = [];
   size = 8;
   numbers: Array<number> = [8, 7, 6, 5, 4, 3, 2, 1];
   letters: Array<string> = [];
+  figures: Array<string> = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'];
+  selected = false;
+  movedFigure = null;
+  movedFigureColor = null;
+  oldSelect = null;
 
   constructor() {
   }
@@ -20,32 +25,52 @@ export class ChessComponent implements OnInit {
       this.letters.push(String.fromCharCode(x));
     }
     console.log('witam');
-    console.table(this.letters);
 
     for (let i = 0; i < this.size; i++) {
       for (let j = 0; j < this.size; j++) {
         if (i % 2 === 0 && j % 2 !== 0 || (i % 2 !== 0 && j % 2 === 0)) {
-          this.tiles.push({ letter: String(this.letters[j]), numeric: String(this.numbers[i]), color: 'black', figure: 'null' });
+          this.tiles.push({ letter: String(this.letters[j]), numeric: String(this.numbers[i]), color: 'black', figure: 'null', figureColor: 'null' });
         } else {
-          this.tiles.push({ letter: String(this.letters[j]), numeric: String(this.numbers[i]), color: 'white', figure: 'null' });
+          this.tiles.push({ letter: String(this.letters[j]), numeric: String(this.numbers[i]), color: 'white', figure: 'null', figureColor: 'null' });
         }
       }
     }
-    const objIndex = this.tiles.findIndex((obj => obj.figure === 'null'));
-    this.tiles[objIndex].figure = 'Chess_rdt45.svg';
-    this.tiles[1].figure = 'Chess_ndt45.svg';
-    this.tiles[2].figure = 'Chess_bdt45.svg';
-    this.tiles[5].figure = 'Chess_bdt45.svg';
-    this.tiles[6].figure = 'Chess_ndt45.svg';
-    this.tiles[7].figure = 'Chess_ndt45.svg';
-    // this.tiles.map((value, index) => value.number === '7');
+    // const objIndex = this.tiles.findIndex((obj => obj.figure === 'null'));
+    // this.tiles[objIndex].figure = 'Chess_rdt45.svg';
+    for (let i = 0; i < this.size; i++) {
+      this.tiles[i].figure = 'Chess_' + this.figures[i] + 'dt45.svg';
+      this.tiles[i].figureColor = 'black';
+    }
+    for (let i = 0; i < this.size; i++) {
+      this.tiles[i + 56].figure = 'Chess_' + this.figures[i] + 'lt45.svg';
+      this.tiles[i + 56].figureColor = 'white';
+    }
 
-
-    this.tiles.map(({ letter, numeric, color, figure }) => ({ letter, numeric, color, figure: numeric === '7' ? 'Chess_pdt45.svg' : null }));
-
-    // for(let i = 0; i<this.size; i++){
-    //
+    // for (let x = 0; x < this.size * this.size; x++) {
+    //   if (this.tiles[x].numeric === '1') {
+    //     this.tiles[x].figure = 'Chess_' + this.figures[x] + 'lt45.svg';
+    //     this.tiles[x].figureColor = 'white';
+    //   }
     // }
+
+
+
+
+    for (let i = 8; i < this.size + 8; i++) {
+      this.tiles[i].figure = 'Chess_pdt45.svg';
+      this.tiles[i].figureColor = 'black';
+    }
+
+
+
+    for (let x = 0; x < this.size * this.size; x++) {
+      if (this.tiles[x].numeric === '2') {
+        this.tiles[x].figure = 'Chess_plt45.svg';
+        this.tiles[x].figureColor = 'white';
+      }
+    }
+
+    console.table(this.tiles);
 
     // const current = this.tiles;
     // this.tiles.next({
@@ -57,5 +82,36 @@ export class ChessComponent implements OnInit {
 
   }
 
-
+  move(i: number) {
+    console.log('KLIKNIĘTO');
+    if (this.selected) {
+      console.log('Teraz wykonamy ruch');
+      console.log('Czy pole ma inny kolor niż figura? ', this.tiles[i].figureColor !== this.movedFigureColor);
+      if (this.tiles[i].figure === 'null'  || this.tiles[i].figureColor !== this.movedFigureColor) {
+        this.tiles[this.oldSelect].figure = 'null';
+        this.tiles[this.oldSelect].figureColor = 'null';
+        this.tiles[i].figure = this.movedFigure;
+        this.tiles[i].figureColor = this.movedFigureColor;
+      }
+      this.oldSelect = null;
+      this.selected = false;
+    } else {
+      if (this.tiles[i].figure === 'null') {
+        console.log('To puste pole!');
+        return;
+      }
+      console.log('Teraz zaznaczono figurę');
+      this.movedFigure = this.tiles[i].figure;
+      this.movedFigureColor = this.tiles[i].figureColor;
+      this.selected = true;
+      this.oldSelect = i;
+    }
+  }
 }
+
+/**
+ * centrowanie obrazków figur
+ * ruchy figur
+ * warunek zwycięstwa
+ * gracz
+ */
