aa8f70c9befbf92e4b251530bdca3d561eb7057c
antkot <antoniks11@gmail.com>
2022-01-18 03:02:43 +0100

    grid

--- src/app/Podstawy/cook-book/part-components/formule/formule.component.ts
+++ src/app/Podstawy/cook-book/part-components/formule/formule.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { AliveState } from '../../../../ActiveState';
 import { tap } from 'rxjs/operators';
-import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-formule',
@@ -11,20 +10,13 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 })
 export class FormuleComponent extends AliveState implements OnInit {
   model = this.fb.group({
-    lists: [['Lista 1', 'Lista 2']],
-    something: this.fb.array([])
+    lists: this.fb.array([])
   });
   constructor(private fb: FormBuilder) {
     super();
   }
-  get something() {
-    return this.model.controls[`something`] as FormArray;
-    // something początkowo jest puste, ale jest eydtowalną tablicą, bez ograniczeń długości
-    // możemy dodać iole chcemy form controli
-    //
-  }
   get lists() {
-    return this.model.controls[`lists`].value as Array<string>;
+    return this.model.controls[`lists`] as FormArray;
   }
 
   ngOnInit(): void {
@@ -44,12 +36,15 @@ export class FormuleComponent extends AliveState implements OnInit {
 addControl() {
     const newForm = this.fb.group({
       name: ['newList', Validators.required],
-      noContent: ['string for now', Validators.required]
+      // noContent: ['string for now', Validators.required]
     });
-    this.something.push(newForm);
+    this.lists.push(newForm);
 }
 delete(index: number) {
-    this.something.removeAt(index);
+    this.lists.removeAt(index);
+  }
+  save() {
+    this.model.controls[`name`].patchValue('Prosiaczek', { emitEvent: true });
   }
 
 
